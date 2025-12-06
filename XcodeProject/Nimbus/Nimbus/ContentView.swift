6c5a2f1da40ee2ef665e9c8f1beb558467aef708
Sedef Bozkurt <sedefbozkurt.4379@gmail.com>
2025-12-06 18:49:35 +0300

    Add OpenWeather API configuration using xcconfig and plist mapping

--- XcodeProject/Nimbus/Nimbus/ContentView.swift
+++ XcodeProject/Nimbus/Nimbus/ContentView.swift
@@ -10,10 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("API KEY: \(Bundle.main.infoDictionary?["API_KEY"] as? String ?? "none")")
         }
         .padding()
     }
